a6cd3f0a3a8beae96a98aef90515123d3238c2a6
noirkotyara <53192867+noirkotyara@users.noreply.github.com>
2020-10-27 08:49:44 +0200

    Les-53-classComponentReact

--- react-start/src/components/Users/Users.jsx
+++ react-start/src/components/Users/Users.jsx
@@ -3,38 +3,47 @@ import React from 'react';
 import User from './User/User';
 import cl from './Users.module.css';
 
-const Users = (props) => {
-let getUsers = () => {
- if (props.usersGenerate.length === 0) {
-       axios.get('https://social-network.samuraijs.com/api/1.0/users')
-        .then(response => {  props.setUsers(response.data.items) });
-}
-};
-   
-
-    let usersGenerate = props.usersGenerate.map(user =>
-        <User key={user.id}
-            id={user.id}
-            name={user.name}
-            photos={user.photos}
-            country='user.country'
-            city='user.city'
-            lastMessage='{user.lastMessage}'
-            statusFollow={user.followed}
-            status={user.status}
-            doFollow={props.doFollow}
-            doUnfollow={props.doUnfollow} />)
-
-    return (
-        <>
+class Users extends React.Component {
+    // constructor(props) {
+    //     super(props);//віддаємо пропси React.Component
+    //     alert('Fuck');
         
-            <div className={cl.menuName}>Users.jsx</div>
-            <button className={cl.butGET} onClick={getUsers}>Get your frinds out here</button>
-            <div className={cl.usersItem}>
-                {usersGenerate}
-            </div>
-        </>
-    );
+    //     //     //пропси перестають бути тим що приходить 
+    //     //     //в компоненту, вони стаються властивістю об'єкту (класу Users), тож викликати їх 
+    //     //     //треба через контекст this
+
+    // }
+
+    getUsers = () => {
+        if(this.props.usersGenerate.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => { this.props.setUsers(response.data.items) });
+
+        }
+    }
+
+    render() {//вертає JSX
+
+        return (
+            <>
+                <div className={cl.menuName}>Users.jsx</div>
+                <button onClick={this.getUsers}>Get friends out of here</button>
+                <div className={cl.usersItem}>
+                    {this.props.usersGenerate.map(user =>
+                        <User key={user.id}
+                            id={user.id}
+                            name={user.name}
+                            photos={user.photos}
+                            country='user.country'
+                            city='user.city'
+                            lastMessage='{user.lastMessage}'
+                            statusFollow={user.followed}
+                            status={user.status}
+                            doFollow={this.props.doFollow}
+                            doUnfollow={this.props.doUnfollow} />)}
+                </div>
+            </>
+        );
+    }
 }
 
 export default Users;
