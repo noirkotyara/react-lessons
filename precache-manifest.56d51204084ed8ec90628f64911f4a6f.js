d122a68afa414fb75f2ed69d02d32148bf48fa21
noirkotyara <noirkotyara@gmail.com>
2020-12-11 21:56:19 +0200

    Updates

---
+++ precache-manifest.56d51204084ed8ec90628f64911f4a6f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a724fcda9d2da9388201eddc7bb61bc",
+    "url": "/react-lessons/index.html"
+  },
+  {
+    "revision": "755e776c02ba0fb6aca8",
+    "url": "/react-lessons/static/css/main.fb46df19.chunk.css"
+  },
+  {
+    "revision": "7af4a95f17a0fff3c47f",
+    "url": "/react-lessons/static/js/2.fd75c70b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-lessons/static/js/2.fd75c70b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "755e776c02ba0fb6aca8",
+    "url": "/react-lessons/static/js/main.d335ef3f.chunk.js"
+  },
+  {
+    "revision": "feb4fc11d378fa304ae0",
+    "url": "/react-lessons/static/js/runtime-main.35748916.js"
+  },
+  {
+    "revision": "ed41fce9f0824cf27d83cc9cdf1416dc",
+    "url": "/react-lessons/static/media/happy.ed41fce9.png"
+  },
+  {
+    "revision": "7bc7e317396b765116af274f0959cf95",
+    "url": "/react-lessons/static/media/kuscherenko.7bc7e317.jpg"
+  },
+  {
+    "revision": "623bb4d99e10301f7a90bdd8f051db2c",
+    "url": "/react-lessons/static/media/logo2.623bb4d9.jpg"
+  },
+  {
+    "revision": "fc2a1a49cf79d6d2594a7ce562a139a5",
+    "url": "/react-lessons/static/media/lyubov.fc2a1a49.jpg"
+  },
+  {
+    "revision": "c04e3ad7b523822d6ae3c32d18439d82",
+    "url": "/react-lessons/static/media/preloader.c04e3ad7.svg"
+  },
+  {
+    "revision": "f356e41d7bfe7bdee4f34d326a8cbb32",
+    "url": "/react-lessons/static/media/sad.f356e41d.png"
+  },
+  {
+    "revision": "097a33adee4631cdc3956330d680c7a4",
+    "url": "/react-lessons/static/media/zorro.097a33ad.jpg"
+  }
+]);
--- react-start/src/components/common/Validators/Validators.jsx
+++
@@ -1,6 +0,0 @@
-export const required = (value) => {
-   return value ? undefined : 'Field is required';
-}
-
-export let maxLengthC = (maxLen) => (value) => {
-   return value && value.length < maxLen ? undefined : `${maxLen} is reached`} 
